11543c3a529a33065086963180d71ffb90421f6b
alisa-shk <demaliska@gmail.com>
2024-03-21 11:03:30 +0200

    hw2-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function checkForSpam(message) {
-    message = message.toLowerCase();
-    return (message.includes("spam") || message.includes("sale"));
+    lowMessage = message.toLowerCase();
+    return (lowMessage.includes("spam") || lowMessage.includes("sale"));
 }
 
 console.log(checkForSpam("Latest technology news")); // false
